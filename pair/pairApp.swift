7c46b9fbc019d201d3487997cdec0e107a5a60f7
Rithika <rithu427@gmail.com>
2023-07-18 09:30:34 -0500

    Initial Commit

---
+++ pair/pairApp.swift
@@ -0,0 +1,17 @@
+//
+//  pairApp.swift
+//  pair
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct pairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
