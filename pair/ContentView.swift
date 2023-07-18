93d0d2ba1c02ea1e9110b493190d9f9c42cba191
emily <eli.mhs24@gmail.com>
2023-07-18 09:45:06 -0500

    I changed ur words

--- pair/ContentView.swift
+++ pair/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Pair Project ")
+            Text("this is emily")
         }
         .padding()
     }
